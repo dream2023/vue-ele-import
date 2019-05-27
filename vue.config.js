36b328f1d91974b5dfb501014aff37d3c9219f46
zhangchaojie <1098626505@qq.com>
2019-05-27 21:26:17 +0800

    更新文档

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,12 @@
 const path = require('path')
 const isDevelopment = process.env.NODE_ENV === 'development'
-const isDoc = process.env.IS_DOC === 'true'
 
 module.exports = {
   publicPath: isDevelopment ? '/' : '/vue-ele-import/',
   outputDir: path.resolve(__dirname, './docs/'),
   css: { extract: isDevelopment },
   configureWebpack: {
-    entry: isDoc ? './documentation/main.js' : './example/main.js',
+    entry: isDevelopment ? './example/main.js' : './documentation/main.js',
     output: {
       libraryExport: 'default'
     }
